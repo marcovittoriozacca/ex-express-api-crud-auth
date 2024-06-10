a56120af78a41b78fe622406f9ffade844194f0c
marcovittoriozacca <zaccamarco01@gmail.com>
2024-06-10 14:49:24 +0200

    feat: user register + hashing password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,28 @@
-const store = (req, res, next) => {
+const { hashPassword } = require('../utils.js');
+require('dotenv').config();
+const prisma = require('../prisma/prismaClient.js');
+
+
+const store = async (req, res, next) => {
     const { email, name, password } = req.body;
-    console.log(email, name, password);
+    try{
+        const hashedPassword = await hashPassword(password + process.env.PEPPER_KEY);
+    
+        const newUser = {
+            email,
+            name,
+            password: hashedPassword,
+        };
 
+        const storeUser = await prisma.user.create({
+            data: newUser,
+        })
 
+    }catch(err){
+        next(err);
+    }
+
+    
 }
 
 module.exports = {
--- utils.js
+++ utils.js
@@ -1,9 +1,17 @@
 const slugify = require('slugify')
+const bcrypt = require('bcrypt');
 
 const makeSlug = (string) => {
     return slugify(string, {lower: true});
 }
 
+const hashPassword = async password => {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    return hashedPassword;
+}
+
+
 module.exports = {
-    makeSlug
+    makeSlug,
+    hashPassword
 }
